efbc929fc514768ba81cee02dd82f37e2205807d
Nitin Budhlakoti <113322720+Codebreaker42@users.noreply.github.com>
2024-04-17 23:00:45 +0530

    nothing changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require('@nomiclabs/hardhat-waffle') //import the chai file
+// require('@nomicfoundation/hardhat-toolbox')
+
 module.exports = {
   solidity: "0.8.9",
 };

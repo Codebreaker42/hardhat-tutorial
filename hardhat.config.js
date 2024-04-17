f9ba3ec84067d200c7d1d228f25db3ee4d98aafc
Nitin Budhlakoti <113322720+Codebreaker42@users.noreply.github.com>
2024-04-18 01:48:38 +0530

    deployement of contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,15 @@
+
 /** @type import('hardhat/config').HardhatUserConfig */
 require('@nomiclabs/hardhat-waffle') //import the chai file
-// require('@nomicfoundation/hardhat-toolbox')
 
+const ALCHEMY_API_KEY="ec_9Cn04gcXVPitK5huGeiTk0PhcHHvQ";
+const SEPOLIA_PRIVATE_KEY="afb7cb1346d8e07d2aab5b4e9e90c113494b44395e44f8c0ba973b71e06d4f37";
 module.exports = {
   solidity: "0.8.9",
+  networks:{
+    sepolia:{
+      url:`https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts:[`${SEPOLIA_PRIVATE_KEY}`],
+    }
+  }
 };
